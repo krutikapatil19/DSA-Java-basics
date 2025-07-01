f27399fd76261d6746b69101142d332a7ed42690
Krutika Vilas Patil <krutikapatil.1911@gmail.com>
2025-07-01 10:39:12 +0530

    Added Kadane's Algorithm code to find the maximum subarray sum in an array

--- Arrays/reverse.java
+++ Arrays/reverse.java
@@ -1,20 +1,20 @@
-public class reverse{
-    public static void main(String[] args){
-        int[] arr={1,2,3,4,5};
+public class reverse{                     // Line 1
+    public static void main(String [] args){
+        int[] arr={2,4,8,10,15};
+
         int i = 0;
-        int j= arr.length -1;
+        int j = arr.length-1;
 
-    while (i<j){
-   int temp=arr[i];
-    arr[i]=arr[j];
-    arr[j]=temp;
+        while(i<j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
 
-    i++;
-    j--;
-    }
-   
-    System.out.println("The reversed array is:");
-     for(int num : arr)
-     System.out.print(num+ " ");
-}
-}
+            i++;
+            j--;
+        }
+        System.out.println("The reversed array is:" );
+        for(int num : arr)
+        System.out.print(num + " ");
+    }                                     
+}                                         
