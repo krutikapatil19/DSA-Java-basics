565ec7fe79b8736cbfb705992663d7c2bbc99638
Krutika Vilas Patil <krutikapatil.1911@gmail.com>
2025-07-03 11:40:12 +0530

    Added a code to find missing number in array

--- Arrays/MoveZeroesToEnd.java
+++ Arrays/MoveZeroesToEnd.java
@@ -1,11 +1,9 @@
 public class MoveZeroesToEnd {
     public static void main(String[] args){
+         int [] arr = {0,1,0,3,0,12};
+         int index = 0 ;             //this index will tell , where to put the next non-zero number
 
-        int [] arr = {0,1,0,3,0,12};
-
-        int index = 0 ;             //this index will tell , where to put the next non-zero number
-
-        //going through the array and pick out all non-zero numbers
+         //going through the array and pick out all non-zero numbers
         for(int i = 0; i<arr.length; i++){
             if (arr[i] != 0){
                 arr[index] = arr[i];             //place the non-zero at the current'index'position
@@ -14,11 +12,11 @@ public class MoveZeroesToEnd {
          }
 
          //now from the 'index' position to the end , fill with 0s
-
          while(index < arr.length) {
             arr[index] = 0;
             index++;
          }
+         
 
          //printing the final array 
          System.out.println("Array after moving zeroes to the end: ");
