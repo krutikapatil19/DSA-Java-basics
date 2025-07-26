61baf2e8acc144cb6b26b0de69cb48401bf8c739
Krutika Vilas Patil <krutikapatil.1911@gmail.com>
2025-07-26 22:57:41 +0530

    StrStrImplementation

--- Strings/StrStrImplementation.java
+++ Strings/StrStrImplementation.java
@@ -3,14 +3,14 @@ public class StrStrImplementation {
         int hLen = haystack.length();
         int nLen = needle.length();
 
-        if(nLen == 0 ) return 0 ;                           //if needle is empty
+        if(nLen == 0 ) return 0 ;                                       //if needle is empty
 
         for(int i =0; i< hLen - nLen; i++){                             //loop where needle can fit  
             if(haystack.substring(i , i+ nLen).equals(needle)){
                 return i ;
             }
         }
-        return -1;                                         //if needle not found
+        return -1;                                                       //if needle not found
     }
         public static void main(String[] args){
             String haystack = "leetcode";
