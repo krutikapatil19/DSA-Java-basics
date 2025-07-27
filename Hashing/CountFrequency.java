2a5b61b4f163a2966ecc3d67f2d623ee78eabbff
Krutika Vilas Patil <krutikapatil.1911@gmail.com>
2025-07-27 19:01:40 +0530

    CountFrequencyOfElements using HashMap

--- Hashing/CountFrequency.java
+++ Hashing/CountFrequency.java
@@ -4,16 +4,15 @@ public class CountFrequency {
 
     //Function to count frequency of elements
     public static HashMap<Integer, Integer> countFrequency(int[] arr) {
-    HashMap<Integer,Integer> map = new HashMap<>();
+    HashMap<Integer,Integer> map = new HashMap<>();                             //created a new HashMap object , named map, it stores key-value pairs.
 
     //Loop through each element in the array 
     for(int num : arr) {
         //If num already exists , increase count by 1. Else , put 1.
-        map.put(num, map.getOrDefault(num, 0) + 1);
-    }
+        map.put(num, map.getOrDefault(num, 0) + 1);                //getOrDefault = Get value if present, else use default.
+        }
     return map;
 }
-
 public static void main(String[] args) {
     int [] arr = {5,7,8,20,8,9,7,8};
 
