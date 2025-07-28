518e842bd65d5451ca6b8bd12eceff4c68f89ebb
Krutika Vilas Patil <krutikapatil.1911@gmail.com>
2025-07-28 17:32:50 +0530

    Check if two Strings are Anagram , using HashMap.

--- Hashing/AnagramCheck.java
+++ Hashing/AnagramCheck.java
@@ -1,5 +1,5 @@
-import java.util.HashMap;
 
+import java.util.HashMap;
 public class AnagramCheck {
     public static boolean isAnagram(String str1, String str2) {
 
@@ -14,16 +14,16 @@ public class AnagramCheck {
         HashMap<Character, Integer> map = new HashMap<>();
 
         // Count characters in str1
-        for (char ch : str1.toCharArray()) {
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        for (char ch : str1.toCharArray()) {                          //get each character from string1
+            map.put(ch, map.getOrDefault(ch, 0) + 1);                 //get how many times the letter was there in the string , if it wasnt there , consider it 0 and add 1 , or increase the count , if it was already there.
         }
 
         // Subtract character counts using str2
-        for (char ch : str2.toCharArray()) {
-            if (!map.containsKey(ch)) return false;
-            map.put(ch, map.get(ch) - 1);
-            if (map.get(ch) == 0) {
-                map.remove(ch);
+        for (char ch : str2.toCharArray()) {                           //get each character from string 2
+            if (!map.containsKey(ch)) return false;                    //if the character is not found in the map , it's not an anagram
+            map.put(ch, map.get(ch) - 1);                              //reduce the count of the character by 1
+            if (map.get(ch) == 0) {                                    //if the character counts become 0
+                map.remove(ch);                                        //remove it from the map
             }
         }
 
