0821d5e397552f4e0909372a98a6932dfef139c9
Krutika Vilas Patil <krutikapatil.1911@gmail.com>
2025-07-29 23:22:37 +0530

    Intersection of TwoArrays

--- Hashing/IntersectionOfTwoArrays.java
+++ Hashing/IntersectionOfTwoArrays.java
@@ -1,6 +1,29 @@
+import java.util.HashMap;
 public class IntersectionOfTwoArrays {
-    public static int Intersection(int[] arr){
+    public static void Intersection(int[] arr1, int[] arr2){
+        HashMap <Integer, Integer> map = new HashMap <> ();
 
+        //Step 1 : Count Frequency of arr1 Elements
+        for(int num : arr1){
+            map.put(num,  map.getOrDefault(num,0) + 1);
+        }
+
+        //Step 2: Check arr2 Elements and print Intersecting Elements
+        
+        for(int num : arr2) {
+            if(map.containsKey(num) && map.get(num) > 0) {
+                System.out.println(num);                        //print common element
+                map.put(num, map.get(num) - 1);                 //reduce frequency
+                }
+            }
+        }
+        public static void main(String[] args) {
+            int[] arr1 = {1,2,2,3};
+            int[] arr2 = {2,2,4,3};
+            
+            //Printing the Intersecting Elements
+            System.out.println("The Intersecting Elements are:");
+            Intersection(arr1, arr2);
+        }
         
     }
-}
