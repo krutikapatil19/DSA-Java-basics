5d16ad27c9448e542c9ceeac55fb224e17e3728d
Krutika Vilas Patil <krutikapatil.1911@gmail.com>
2025-07-30 16:15:40 +0530

    Added Linear Search Implementation in java

---
+++ Searching/LinearSearch.java
@@ -0,0 +1,24 @@
+public class LinearSearch {
+    
+    public static int LinearSearching(int[] arr, int key) {
+
+        for(int i =0; i<arr.length; i++){
+            if(arr[i] == key){
+                return i;                                       //returns the index of the element foun
+            }
+        }
+        return -1;                                             //return -1 if the element not found                     
+    }
+    public static void main (String[] args){
+        int [] arr = {2, 4, 6, 10, 3};
+        int key = 6;
+
+        int result = LinearSearching(arr,key);
+
+        if(result != -1){
+            System.out.println("The element found at : " + result);
+        }else {
+            System.out.println("Element not Found!");
+        }
+    }
+}
