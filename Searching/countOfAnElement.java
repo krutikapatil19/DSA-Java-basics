6ba141828b44bf74f0de22da09f4bec652134afb
Krutika Vilas Patil <krutikapatil.1911@gmail.com>
2025-07-31 17:42:03 +0530

    Count of an Element in an Array

--- Searching/countOfAnElement.java
+++ Searching/countOfAnElement.java
@@ -3,23 +3,23 @@ public class countOfAnElement {
     //Find First Occurence
     public static int firstOccurrence(int[] arr , int key){
 
-        int start = 0;
-        int end = arr.length - 1;
+        int start = 0;                                                  //starting index
+        int end = arr.length - 1;                                       //ending index
 
         while(start <= end){
-            int mid = (start + end) / 2;
+            int mid = (start + end) / 2;                                //calculate mid index
 
-            if(key == arr[mid] && (mid == 0 || arr[mid -1] != key)){
-                return mid;
+            if(key == arr[mid] && (mid == 0 || arr[mid -1] != key)){    //check if the mid is the first occurence of the key
+                return mid;                                             //return index , if the first occurence found
             }
-            else if(key <= arr[mid]){
+            else if(key <= arr[mid]){                                   //if key is smaller or equal , move left
                 end = mid-1 ;
             }
             else {
-                start = mid + 1;
+                start = mid + 1;                                        //if key is greater , move right
             }
         }
-        return -1;
+        return -1;                                                      //key not found.
     }
     
 
@@ -28,20 +28,20 @@ public class countOfAnElement {
         int start = 0;
         int end = arr.length - 1;
 
-        while(start <= end){
-            int mid = (start+end)/2;
+        while(start <= end){                                            //binary search loop 
+            int mid = (start+end)/2;                                    //calculate mid index
 
             if(key == arr[mid] && (mid == arr.length - 1 || arr[mid+1] != key)){
-                return mid;
+                return mid;                                            //return index , if the last occurence found
             }
-            else if (key < arr[mid]){
+            else if (key < arr[mid]){                                  //if key is smaller , search left
                 end = mid - 1;
             }
             else{
-                start = mid + 1;
+                start = mid + 1;                                       //if key is greater , search right
             }
         }
-        return -1;
+        return -1;                                                    //element not found
     }
 public static void main(String[] args){
     int [] arr = {2, 4, 4, 4, 5, 7, 9, 11};
