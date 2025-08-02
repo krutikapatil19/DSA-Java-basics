c28d51c7aff5810ad34156ffbde2f8fd0794f1ca
Krutika Vilas Patil <krutikapatil.1911@gmail.com>
2025-08-02 21:30:58 +0530

    Factorial of a Number

---
+++ Recursion/Factorial.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class Factorial {
+    public static int factorial(int n) {
+        //Base Case
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        //Recursive Case
+        return n* factorial(n-1);
+    }
+    public static void main(String[] args) {
+        int num = 4;
+        System.out.println("Factorial of " + num   +  " is : " + factorial(num));
+    }
+}
