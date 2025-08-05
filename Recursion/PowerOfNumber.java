cf4e73f8383f02937def67a34d699a5fc8cf8c6a
Krutika Vilas Patil <krutikapatil.1911@gmail.com>
2025-08-05 10:30:55 +0530

    Power of a Number using Recursion.

---
+++ Recursion/PowerOfNumber.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class PowerOfNumber {
+    public static int power(int x, int n){
+
+        //Base Case: anything^0 = 1
+        if(n==0){
+            return 1;
+        }
+        //Recursive Case: x * (x^(n-1))
+        return x * power(x, n-1);                          //multiply once , and let recursion do the rest.
+    }
+    public static void main(String[] agrs){
+        int x = 2 , n = 4 ;
+    System.out.println("The power of the number x is : " + power(x,n) );
+    }
+    
+}
