2bbcc280c2f3840893027f89ff194821f2cd039a
Krutika Vilas Patil <krutikapatil.1911@gmail.com>
2025-08-10 13:26:15 +0530

    Three Sum implementation.

--- Arrays/ThreeSum.java
+++ Arrays/ThreeSum.java
@@ -6,15 +6,15 @@ public class ThreeSum {
         Arrays.sort(nums);                                                              //sorts the array , Sorting helps use two pointer technique , easily skip duplicate triplets.
 
 
-        for(int i =0; i< nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) continue;
+        for(int i =0; i< nums.length - 2; i++) {                                        //loops through all the elements of array , till nums.length - 2 .  because we need atleast 2 more elmenets after the i .
+            if (i > 0 && nums[i] == nums[i-1]) continue;                                //skip duplicate values for the first element of the triplet ,
             
 
-            int left = i + 1, right = nums.length - 1;
-            while(left < right) {
+            int left = i + 1, right = nums.length - 1;                                  //we keep two pointer over here , left starts just after i , right starts at the end of the array
+            while(left < right) {                                                       //move right and left inward , to find pairs with nums[i] sum to zero.   //while two pointers haven't crossed , keep searching for valid pairs.
                 int sum = nums[i] + nums[left] + nums [right];
 
-                if(sum == 0) {
+                if(sum == 0) {                                                              //if sum is zero , we found a valid triplet .
                     res.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     while(left < right && nums[left] == nums[left+1]) left++;
                     while(left < right && nums[right] == nums[right-1]) right --;
