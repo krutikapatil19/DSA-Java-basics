5c1789c8ccc8a38782f0cab4e138181390db6554
Krutika Vilas Patil <krutikapatil.1911@gmail.com>
2025-08-10 13:17:05 +0530

    Three Sum implementation

--- Arrays/ThreeSum.java
+++ Arrays/ThreeSum.java
@@ -1,36 +1,42 @@
 import java.util.*;
 public class ThreeSum {
-    
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(nums);
 
-        for(int i =0; i<nums.length - 2; i++) {
+    public List<List<Integer>> threeSum(int[] nums) {                                   //defined a method threeSum that , takes an integer array nums as input  //And returns list of triplets(List<List<Integer>>), where each triplet equals to 0.
+        List<List<Integer>> res = new ArrayList<>();                                    //creates an empty list res to store the answer triplets(each triplet is a List<Integer>).
+        Arrays.sort(nums);                                                              //sorts the array , Sorting helps use two pointer technique , easily skip duplicate triplets.
+
+
+        for(int i =0; i< nums.length - 2; i++) {
             if (i > 0 && nums[i] == nums[i-1]) continue;
+            
 
             int left = i + 1, right = nums.length - 1;
             while(left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
+                int sum = nums[i] + nums[left] + nums [right];
 
-                if (sum == 0) {
+                if(sum == 0) {
                     res.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    while ( left < right && nums[left] == nums[left +1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right --;
+                    while(left < right && nums[left] == nums[left+1]) left++;
+                    while(left < right && nums[right] == nums[right-1]) right --;
                     left++;
                     right--;
                 } else if (sum < 0) {
-                    left ++;
+                    left++;
                 } else {
                     right --;
                 }
             }
 
+
+
         }
         return res;
     }
+    
     public static void main(String[] args) {
         ThreeSum ts = new ThreeSum();
-        int[] nums = {-1, 0, 1, 2, -1, -4};
+        int[] nums = {-1,0,1,3,2,-3,1};
         System.out.println(ts.threeSum(nums));
     }
+    
 }
