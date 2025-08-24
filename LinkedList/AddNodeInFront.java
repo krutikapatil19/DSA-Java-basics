f9fcb9776b68d532035d9551503908100b089942
Krutika Vilas Patil <krutikapatil.1911@gmail.com>
2025-08-24 13:59:49 +0530

    Add Node in front , in a linked list.

--- LinkedList/AddNodeInFront.java
+++ LinkedList/AddNodeInFront.java
@@ -16,17 +16,17 @@ public class AddNodeInFront {
     //Insert in beginning
     public void insert(int data) {
         Node newNode = new Node(data);          //create a new node
-        newNode.next = head;                   //the new node become head , and rest list move forward
-        head = newNode;
+        newNode.next = head;                    //the new node become head , and rest list move forward
+        head = newNode;                         //updating head to new node , as it is inserted in front
     }
 
 
     //Print Linked List
     public void display() {
-        Node temp = head;
-        while(temp != null) {
+        Node temp = head;                       //assign head to temp, for traversal of linkedlist from head to tail
+        while(temp != null) {                   //traversed until the next node(temp) is null 
             System.out.print(temp.data + " -> ");
-            temp = temp.next;
+            temp = temp.next;                   //update temp , to display nodes one by one 
         }
         System.out.print("null");
     }
