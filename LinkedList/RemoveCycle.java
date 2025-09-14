b0b726b0d9039c12a2d9cbc70c9d1f576bd5e61c
Krutika Vilas Patil <krutikapatil.1911@gmail.com>
2025-09-14 14:31:54 +0530

    Remove the cycle in linked list

--- LinkedList/RemoveCycle.java
+++ LinkedList/RemoveCycle.java
@@ -35,7 +35,7 @@ public class RemoveCycle {
             slow = slow.next;
             fast = fast.next;
         }
-        // Now slow or (fast) is at the start of the cycle
+        // Now slow (or fast) is at the start of the cycle
 
         // Step 3: Find the last node of cycle and break it
         ListNode temp = slow;
@@ -43,5 +43,34 @@ public class RemoveCycle {
             temp = temp.next;
         }
         temp.next = null; // breaks the cycle
+    }
+
+    // Helper function to print the linked list
+    public static void printList(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        // Create a linked list
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        // Create a cycle: connect 5 -> 3
+        head.next.next.next.next.next = head.next.next;
+
+        // Detect & remove cycle
+        removeCycle(head);
+
+        // Print list after removing cycle
+        System.out.println("Linked List after removing cycle:");
+        printList(head);
     }
 }
