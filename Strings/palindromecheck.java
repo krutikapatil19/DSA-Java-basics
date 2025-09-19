34846ac9aa60b69b89421fd23c8de7b36547f398
krutikapatil19 <krutikapatil.1911@gmail.com>
2025-09-19 18:14:07 +0530

    palindrome Check

--- Strings/palindromecheck.java
+++ Strings/palindromecheck.java
@@ -1,26 +1,35 @@
-public class palindromecheck {
-    public static void main(String [] args){
-        String str = "Madam";
+public class PalindromeCheck {
+    public boolean isPalindrome(String str) {
 
-        str = str.toLowerCase();            //makes i and j comparison easy if all letters are lowercase
+        // str = str.toLowerCase(); //makes i and j comparison easy if all letters are
+        // lowercase
 
-        int i = 0;                          //2 pointers are used .i=start , and j =end of string , to traverse from both sides
-        int j = str.length()-1;
-        boolean isPalindrome= true;
+        int i = 0;          // 2 pointers are used .i=start , and j=last element , to traverse from both sides.
+        int j = str.length() - 1;
+        boolean isPalindrome = true;
 
-        while(i<j){                                 //traversal is done from both ends until it reaches the both mid values which are next to each other
-            if(str.charAt(i) !=str.charAt(j)){          //to check both values , if the are not equal, string!=palindrom
-                 isPalindrome = false;
-                 break;
+        while (i < j) {     // traversal is done from both ends until it reaches the both mid values which
+                            // are next to each other
+            if (str.charAt(i) != str.charAt(j)) {           // to compare both values , if not then string is not palindrome.  
+                isPalindrome = false;
+                break;
             }
             i++;
             j--;
         }
-        if(isPalindrome){
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str = "madam";
+        PalindromeCheck obj = new PalindromeCheck();
+        obj.isPalindrome(str);
+
+        if (obj.isPalindrome(str)) {
             System.out.println("The string is Palindrome");
-        }else{
-            System.out.println("The String is not palindrome");
+        } else {
+            System.out.println("The String is not Palindrome");
         }
     }
-    
+
 }
