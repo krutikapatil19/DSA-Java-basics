955e43296fd7a6b06e13af05dc6d35d845341b57
krutikapatil19 <krutikapatil.1911@gmail.com>
2025-09-21 10:29:29 +0530

    Character Frequency count in string.

--- Strings/CharacterFrequency.java
+++ Strings/CharacterFrequency.java
@@ -5,11 +5,11 @@ public class CharacterFrequency {
     str = str.toLowerCase();
     int[] freq = new int[256];                                  //Array to store frequency , ASCII 256 possibilites
 
-    for(int i =0; i< str.length(); i++){
+    for(int i =0; i< str.length(); i++){                        //This for loop counts the frequencies , but does not print it.
         freq[str.charAt(i)] ++;                                 //Increases the count for character at ith index.
     }
 
-    for(int i =0; i< 256; i++) {
+    for(int i =0; i< 256; i++) {                                //This for loop goes through all ASCII codes (0-255),only print characters that appeared.
         if (freq[i] > 0) {
             System.out.println((char)i + " : " + freq[i]);      //(char) i -converts ASCII number back to actual character.
         }
