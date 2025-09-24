e48932a6f165c45bb3598324bee56d7feebef91b
krutikapatil19 <krutikapatil.1911@gmail.com>
2025-09-24 09:38:54 +0530

    Move Zeroes to the end.

--- Arrays/MoveZeroesToEnd.java
+++ Arrays/MoveZeroesToEnd.java
@@ -1,4 +1,36 @@
 public class MoveZeroesToEnd {
+
+   public static void movingZeroesToEnd(int[] arr){
+      int index = 0 ;
+
+      for(int i =0; i<arr.length; i++){
+         if(arr[i]!=0){
+            arr[index++] = arr[i];                    //Move non-zero numbers forward.
+         }
+      }
+      //Fill the rest with zeroes
+      while(index<arr.length) {
+         arr[index++] =0;
+      }
+   }
+
+      public static void main(String[] args){
+         int[] arr = {3,8,0,5,18,0,9};
+         movingZeroesToEnd(arr);
+         System.out.println("Array after moving zeroes to the end : ");
+
+         for(int i =0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+
+         }
+      }
+   }
+
+
+/* 
+
+
+public class MoveZeroesToEnd {
     public static void main(String[] args){
          int [] arr = {0,1,0,3,0,12};
          int index = 0 ;             //this index will tell , where to put the next non-zero number
@@ -25,3 +57,4 @@ public class MoveZeroesToEnd {
          }
 }
 }
+*/
