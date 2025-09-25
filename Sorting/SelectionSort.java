a7ff594f884f3474f732aa37c7be7e659f33f696
krutikapatil19 <krutikapatil.1911@gmail.com>
2025-09-25 10:32:15 +0530

    Selection sort implementation.

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,23 +1,23 @@
 public class SelectionSort {
     public static  void selectionSort(int[] arr){
-        int n = arr.length;
 
         // Outer loop 
-        for(int i = 0; i < n - 1; i++){
-            int smallest = i;                               // assume the current element is the smallest
+        for(int i = 0; i<arr.length-1; i++){
+            int smallest = i;                             // assume the current element is the smallest
 
             // Inner loop - find the index of the smallest element in the remaining array
-            for(int j = i+1; j<n; j++){   
+            for(int j = i+1; j<arr.length; j++){   
                 if(arr[smallest] > arr[j]) {              // if a smaller element is found, update smallest
                     smallest = j;
             }
+        }
         
-            int temp = arr[smallest];                       // Swap the smallest found element with the element at index i
+            int temp = arr[smallest];                     // Swap the smallest found element with the element at index i
             arr[smallest] = arr[i];
             arr[i] = temp;
         }
     }
-}
+
     public static void main(String[] args){
         int[] arr = {20, 22, 12, 9, 90, 5};
         selectionSort(arr);
