8e4a7c2951a98317b7d65e588977c6a40e6ce6f0
krutikapatil19 <krutikapatil.1911@gmail.com>
2025-09-25 10:04:30 +0530

    Bubble sort implementation.

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,15 +1,13 @@
 public class BubbleSort {
     public static void bubbleSort(int[] arr){
        
-        int n = arr.length ;
-        for(int i =0; i< n ; i++){                          //outer loop (iterating through the array)
+        for(int i =0; i< arr.length-1 ; i++){               //outer loop (iterating through the array)
 
-        for(int j = 0; j < n - i - 1; j++) {                //inner loop (checks for the adjacent elements)
-
+        for(int j = 0; j < arr.length-i-1; j++) {           //inner loop (checks for the adjacent elements)
             if(arr[j] > arr[j+1]){                          //Swap if the elements are in wrong order
             int temp = arr[j];
             arr[j] = arr[j+1];
-            arr[j + 1] = temp;
+            arr[j+1] = temp;
         }
       }            
     }
@@ -18,9 +16,9 @@ public static void main(String [] args){
     int[] arr = {4,5,7,9,10,3,12};
 
     bubbleSort(arr);                                        //calling the method 
-    
-for(int num : arr){                                         //print sorted array 
-    System.out.print(num + " " );
+    for(int i = 0; i<arr.length; i++){
+        System.out.print(arr[i] + " ");                     //print sorted array 
+    }                                         
+  }
 }
-}
-}
+
