a27db34704fdc68557340bbffa64552ab3eda4cc
krutikapatil19 <krutikapatil.1911@gmail.com>
2025-09-26 11:29:10 +0530

    Pyramid pattern .

--- Pattern_Problems/pyramidPattern.java
+++ Pattern_Problems/pyramidPattern.java
@@ -1,12 +1,12 @@
 public class pyramidPattern {
     public static void main(String[] args) {
-        int n = 5; 
+        int n = 4; 
 
-        for (int i = 1; i <= n; i++) {
-            for (int space = 1; space <= n - i; space++) {
+        for (int i = 0; i <= n; i++) {                              //outer loop - for printing n rows    
+            for (int s=1; s<= n-i; s++) {                           //to print spaces before stars.
                 System.out.print(" ");
             }
-            for (int star = 1; star <= (2 * i - 1); star++) {
+            for (int star = 1; star <= (2 * i + 1); star++) {       //stars in each row  - formula - 
                 System.out.print("*");
             }
 
