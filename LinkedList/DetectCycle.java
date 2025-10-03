e21158877b0478cd45a637e2ed4a4ca61e86ad0a
krutikapatil19 <krutikapatil.1911@gmail.com>
2025-10-03 19:01:19 +0530

    Detect Cycle in Linkedlist.

--- LinkedList/DetectCycle.java
+++ LinkedList/DetectCycle.java
@@ -1,43 +1,43 @@
 class Node {
-int val;                                            //value inside the node
-Node next;                                          //reference to the next node
-Node(int val) {
-    this.val = val;                                 //put value in the node
-    this.next = null;                               
+    int val;                                    //value inside the node
+    Node next;                                  //reference to the next node 
+    Node(int val) {
+        this.val = val;                         //put value in the node 
+        this.next = null;
     }
 }
-
 public class DetectCycle {
-    //Function to check if cycle is present
+    //Function to check if cycle is present 
     public static boolean hasCycle(Node head) {
-        Node slow = head,                           //slow pointer -> moves 1 step
-        fast = head;                                //fast pointer -> moves 2 steps
-        while(fast!= null && fast.next!= null){     //keep moving till fast reaches end
-            slow = slow.next;                       //move slow 1 step
-            fast = fast.next.next;                  //move fast 2 steps
-            if(slow == fast) return true;           //if both meet -> cycle
+        Node slow = head;                                   //Initially slow pointer and fast pointer , both points towards head Node. 
+        Node fast = head;                                   
+        while(fast!= null && fast.next !=null){             //keep moving till fast reaches end
+            slow = slow.next;                               //slow moves 1 step
+            fast= fast.next.next;                           //fast pointer moves 2 steps
+            if(slow == fast) return true;                   //if both meet -> cycle exists
         }
-        return false;                               //if fast reaches null -> no cycle
+        return false;                                       //if fast reaches null , no cycle exists.
     }
+
+    public static void main(String [] args) {
+        //Creating nodes
+        Node n1 = new Node(2);
+        Node n2 = new Node(5);
+        Node n3 = new Node(10);
+        Node n4 = new Node(16);
+        Node n5 = new Node(26);
+
+        //Connecting the nodes
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n3;
     
-public static void main(String[] args){
-    //Create nodes
-    Node n1 = new Node(1);
-    Node n2 = new Node(2);
-    Node n3 = new Node(6);
-    Node n4 = new Node(20);
-    Node n5 = new Node(12);
-
-    //connecting the nodes : 1 -> 2 -> 3 -> 4 -> 5
-    n1.next = n2;
-    n2.next = n3;
-    n3.next = n4;
-    n4.next = n5;
-
     //Check cycle
-    if (hasCycle(n1))
-    System.out.println("Cycle exists");
-    else 
-    System.out.println("No cycle");
-}
+    if(hasCycle(n1))
+    System.out.println("Cycle exists.");
+    else
+        System.out.println("Cycle doesn't exist.");
+    
+    }
 }
