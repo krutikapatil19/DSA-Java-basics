d47e82a37cec8df9988b5ce50fb08aa89c970999
krutikapatil19 <krutikapatil.1911@gmail.com>
2025-10-03 10:36:13 +0530

    Reverse the LinkedList using Iterative Approach.

--- LinkedList/ReverseLinkedList.java
+++ LinkedList/ReverseLinkedList.java
@@ -1,34 +1,77 @@
 class Node{                                                             //we defined what a node in a linked list looks like 
-    int val;                                                            //the number/value stored in the box.
+    int val;                                                            //value stored in the box.
     Node next;                                                          //A pointer to the next box. If there's no box , then its null.
-    Node(int val) {this.val = val;}                                     //Constructor , when a new node is created , it saves the value .
+    Node(int val) {                                                     //Constructor 
+        this.val = val;         
+    }                                                
 }
 
 public class ReverseLinkedList {
+    Node head;
 
-    //print linked list
-    static void printList(Node head) {                                  //static is used , so that we can call it directly from main , without maiking an object.  //printList(Node head) = when we give the first node , it will print it.
-        for (Node temp = head; temp != null; temp = temp.next)
-        System.out.print(temp.val + " -> ");
-        System.out.println("null");                                   //shows the end of the chain.
+    //Method to add a node at the end
+    public void addLast(int val){
+        Node newNode = new Node(val);
+        if(head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = newNode;
     }
 
-    
-    static Node insertEnd(Node head, int val) {                         //add a new node with val , at the end of the list.
-        if (head == null) return new Node(val);                         //if list is empty , the new node becomes the head.
-        Node temp = head;                                               //starts from the head node 
-        while (temp .next != null) temp = temp.next;                    //move forward , until you reach the last node(the one whose next is null).
-        temp.next = new Node(val);                                      //attaches the new node at the end 
-        return head;
+    //Iterative approach to reverse the list
+    public void reverseIterative(){
+    if(head == null ||head.next == null){
+        return;
+    }
+
+    Node prevNode = head;
+    Node currNode = head.next;
+
+    while(currNode!= null){
+        Node nextNode = currNode.next;
+        currNode.next = prevNode;
+
+        //update
+        prevNode = currNode;
+        currNode = nextNode;
+    }
+    head.next = null;
+    head = prevNode;
+}
+
+    //helper method to print list
+    public void printList() {
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("NULL");
     }
 
     public static void main(String[] args) {
-        Node head = null;
-        head = insertEnd(head, 1);
-        head = insertEnd(head, 2);
-        head = insertEnd(head, 3);
-        head = insertEnd(head, 4);
+        ReverseLinkedList list = new ReverseLinkedList();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(5);
+        list.addLast(10);
 
-        printList(head);
+        //Print original list
+        System.out.println("Original list : ");
+        list.printList();
+
+        //Reverse the list
+        list.reverseIterative();
+
+        //Print reversed list
+        System.out.println("Reversed list: ");
+        list.printList();
     }
 }
+
+    
--- Strings/palindromecheck.java
+++ Strings/palindromecheck.java
@@ -16,6 +16,7 @@ public class PalindromeCheck {
             }
             i++;
             j--;
+            
         }
         return true;
     }
