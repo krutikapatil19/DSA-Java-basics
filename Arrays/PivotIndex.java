4a0579d762491851460b1d5b01353d56f7b3530e
krutikapatil19 <krutikapatil.1911@gmail.com>
2025-10-05 11:46:46 +0530

    add to ArrayForm(digit-by-digit approach).

--- Arrays/PivotIndex.java
+++ Arrays/PivotIndex.java
@@ -1,3 +1,5 @@
+//Leetcode 724:
+//Given an array of integers nums, calculate the pivot index of this array.The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
 public class PivotIndex {
     public static int pivotIndex(int[] arr ){
         int total = 0;
