b4f10681090af091d7430d1f4c1fb760db93822f
krutikapatil19 <krutikapatil.1911@gmail.com>
2025-10-10 11:57:30 +0530

    Chocolate distribution problem.

--- Arrays/ChocolateDistribution.java
+++ Arrays/ChocolateDistribution.java
@@ -1,5 +1,64 @@
-
 //Chocolate Distribution Problem(GOG)
+import java.util.Arrays;
+public class ChocolateDistribution {
+    public static int chocoDistribution(int[] arr, int m){
+    Arrays.sort(arr);
+    int MinDiff = Integer.MAX_VALUE;
+    int startIndex = 0;
+    int diff = 0;
+
+    for(int i =0;i<arr.length-m;i++){
+        //diff = maxElement - minElement
+        diff = arr[i+m-1] -arr[i];
+
+        if(diff<MinDiff) {
+            MinDiff = diff;
+            startIndex = i;
+        }
+    }
+    //Print the packets and their indices
+    System.out.print("Chosen packets : ");
+    for(int i = startIndex; i<startIndex+m; i++){
+        System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+
+    //Print indices of the packet 
+    System.out.print("Indices of the packets : ");
+    for(int i = startIndex; i<startIndex+m; i++){
+        System.out.print(i+ " ");
+    }
+    System.out.println();
+    return MinDiff;
+}
+public static void main(String[] args){
+    int[] arr = {3,12,16,50,51,52};
+    int m = 3;
+    System.out.println(chocoDistribution(arr,m));
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
 import java.util.Arrays;
 
 public class ChocolateDistribution {
@@ -10,7 +69,7 @@ public class ChocolateDistribution {
         int middleIndex = 0;
         int endIndex = 0;
         // Slide the window
-        for (int i = 0; i + k - 1 < arr.length; i++) {
+        for (int i = 0; i<= arr.length-k; i++) {
             int diff = arr[i + k - 1] - arr[i]; // difference between max and min .
 
             if (diff < minDiff) { // update minDiff
@@ -38,3 +97,4 @@ public class ChocolateDistribution {
         }
     }
 }
+*/
