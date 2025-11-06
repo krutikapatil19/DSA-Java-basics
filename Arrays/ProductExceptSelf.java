27e77c15ca2f74cb03173e522409b4d60ef53a30
krutikapatil19 <krutikapatil.1911@gmail.com>
2025-11-06 10:11:22 +0530

    Solved Product of Array Except Self using prefix-suffix approach.

--- Arrays/ProductExceptSelf.java
+++ Arrays/ProductExceptSelf.java
@@ -1,33 +1,35 @@
-public class ProductExceptSelf{
-    public static int[] product(int[] nums){
+import java.util.Arrays;
+
+public class ProductExceptSelf {
+    public static int[] product(int[] nums) {
         int n = nums.length;
-        int []LP = new int[n];          //left products
-        int []RP = new int[n];          //right products
-        int[] answer = new int[n];
-        
-        //left product array 
-        LP[0] = 1;              //nothing is to the left of first element
-        for (int i = 1; i<n; i++) {
-            LP[i] = LP[i-1]*nums[i-1];
+        int[] leftProduct = new int[n]; // left products
+        int[] rightProduct = new int[n]; // right products
+        int[] answer = new int[n]; // product of leftProduct and rightProduct
+
+        // left product array
+        leftProduct[0] = 1; // nothing is to the left of first element
+        for (int i = 1; i < n; i++) {
+            leftProduct[i] = leftProduct[i - 1] * nums[i - 1];
         }
-        //right product array
-        RP[n-1] = 1;//nothing to the right of last element
-        for(int i = n-2; i>=0;i--){
-        RP[i] = RP[i+1] * nums[i+1];
+        // right product array
+        rightProduct[n - 1] = 1;// nothing to the right of last element
+        for (int i = n - 2; i >= 0; i--) {
+            rightProduct[i] = rightProduct[i + 1] * nums[i + 1];
         }
 
-        //Multiply left and right products
-        for(int i = 0; i<n; i++){
-            answer[i] = LP[i] *RP[i];
+        // Multiply left and right products
+        for (int i = 0; i < n; i++) {
+            answer[i] = leftProduct[i] * rightProduct[i];
         }
         return answer;
     }
+
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4};
+        int[] nums = { 1, 2, 3, 4 };
         int[] result = product(nums);
 
-        for(int x : result) {
-            System.out.print(x + " ");
-        }
+        System.out.println(Arrays.toString(result));
+
     }
 }
