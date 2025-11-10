8a29d8ba7085f8959b4be72d55147e44e5415dfd
krutikapatil19 <krutikapatil.1911@gmail.com>
2025-11-10 14:46:56 +0530

    Three Sum Implementation.

--- Arrays/ThreeSum.java
+++ Arrays/ThreeSum.java
@@ -4,14 +4,13 @@ public class ThreeSum {
 
     public List<List<Integer>> threeSum(int[] nums) { // defined a method threeSum that , takes an integer array nums as input And returns list of triplets (List<List<Integer>>),
                                                       // where each triplet sum equals to 0.
-        List<List<Integer>> result = new ArrayList<>(); // creates an empty list res to store the answer triplets (each
-                                                        // triplet is a List<Integer>).
+        List<List<Integer>> result = new ArrayList<>(); // creates an empty list res to store the answer triplets (each triplet is a List<Integer>).
         if (nums == null || nums.length < 3) {
             // If fewer than 3 numbers, can't form a triplet
             return result;
         }
 
-        Arrays.sort(nums); // Sorts array for two pointer technique.
+        Arrays.sort(nums); // For two pointer technique.
 
         int n = nums.length;
         for (int i = 0; i < n - 2; i++) { //till n - 2. because we need atleast 2 more elements after the i .
@@ -27,8 +26,7 @@ public class ThreeSum {
                 if (sum == 0) { 
                     result.add(List.of(nums[i], nums[left], nums[right])); //store that triplet(whose sum ==0) in res , res acts as a basket .
                     while (left < right && nums[left] == nums[left + 1])
-                        left++; // we check if the left pointer element is same as element at left pointer+1 ,
-                                // if its same , then we skip it , i.e. we do left++ , as we want unique triplets , not two same triplets
+                        left++; // if its same , then we skip it , i.e. we do left++ , as we want unique triplets , not two same triplets
 
                     while (left < right && nums[right] == nums[right - 1])
                         right--; // same as above case
@@ -37,8 +35,7 @@ public class ThreeSum {
                     right--;
                 } else if (sum < 0) { // if the sum is lesser than 0 , then we need bigger elements , so we do left++ as the array is sorted .
                     left++;
-                } else { // if the sum is greater than 0 , then we do right-- , as we want a smaller
-                         // element .
+                } else { // if the sum is greater than 0 , then we do right-- , as we want a smaller element .
                     right--;
                 }
             }
