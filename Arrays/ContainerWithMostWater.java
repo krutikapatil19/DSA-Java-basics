91693391955b8a8dbc60f0e7a2fba40cc61d0687
krutikapatil19 <krutikapatil.1911@gmail.com>
2025-11-11 22:24:39 +0530

    Container with Most Water (two pointer approach).

--- Arrays/ContainerWithMostWater.java
+++ Arrays/ContainerWithMostWater.java
@@ -1,32 +1,34 @@
 public class ContainerWithMostWater {
     public static int maxArea(int[] height) {
         int left = 0;
-        int right = height.length -1;
-        int maxArea = 0;//keeps track of the largest water area found so far.
+        int right = height.length - 1;
+        int maxArea = 0;// keeps track of the largest water area found so far.
 
-        while(left<right) {
-            int width = right - left;
-            int h = Math.min(height[left], height[right]);
-            int currentArea = width * h;
-            if(currentArea > maxArea) {
+        while (left < right) {
+            int width = right - left;// distance between two lines
+            int heightt = Math.min(height[left], height[right]);// the height of water depends upon shorter line,because water can't go above it.
+            int currentArea = width * heightt;
+            if (currentArea > maxArea) {
                 maxArea = currentArea;
             }
 
-            //Moving the pointer pointing to the shorter line inward
+            // Moving the pointer pointing to the shorter line inward
             if (height[left] < height[right]) {
                 left++;
-            }else {
+            } else {
                 right--;
             }
         }
         return maxArea;
     }
     public static void main(String[] args) {
-        int[] height = {1,8,6,2,5,4,8,3,7};
+        int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
         System.out.println(maxArea(height));
     }
 }
-/*Complexitites 
-Time: O(n)
-Space: O(1)
-*/
+
+/*
+ * Complexitites
+ * Time: O(n)
+ * Space: O(1)
+ */
