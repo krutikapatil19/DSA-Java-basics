fcb644fe7184086025b32cd150d7dd0da511b9d1
krutikapatil19 <krutikapatil.1911@gmail.com>
2025-11-17 18:25:28 +0530

    Minimum Merge Operations needed to make an Array Palindrome.

--- Arrays/MinimumMergeOperations.java
+++ Arrays/MinimumMergeOperations.java
@@ -2,21 +2,28 @@ public class MinimumMergeOperations {
     public static int minimumMergeOperationsToMakeAnArrayPalindrome(int[]arr){
         int left = 0;
         int right = arr.length-1;
-        int count = 0;
+        int count = 0;//no. of merge operations done 
 
         while(left<right){
-            if(arr[left] == arr[right]){
+            //CASE 1: Both elements are equal -> already matching pair
+            if(arr[left] == arr[right]){ 
                 left++;
                 right--;
             }
+            //CASE 2: left element < right element -> merge on left side 
             else if(arr[left]<arr[right]){
-                arr[left+1]=arr[left]+arr[left+1];//merge 
-                left++;
-                count++;
+
+                //merge arr[left] with arr[left+1] 
+
+                arr[left+1]=arr[left]+arr[left+1];
+                left++;//move pointer 
+                count++;//merged 
             }else {
+
+                //CASE 3: Right value is smaller , merge arr[right] with arr[right-1]
                 arr[right-1]=arr[right]+arr[right-1];
-                right--;
-                count++;
+                right--;//move pointer
+                count++;//merged
             }
         }
         return count;
