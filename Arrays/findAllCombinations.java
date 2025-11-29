9d5b4b4347d2de1456d809c0f145e5464717d375
krutikapatil19 <krutikapatil.1911@gmail.com>
2025-11-29 22:30:02 +0530

    Find all combinations of size r from an array

--- Arrays/findAllCombinations.java
+++ Arrays/findAllCombinations.java
@@ -1,33 +1,39 @@
 import java.util.*;
 
-public class findAllCombinations{
-    public static void printCombinations(int[] arr, int r) {
-    List<Integer> temp = new ArrayList<>();
-    generate(arr, 0, r, temp);
-}
-private static void generate(int[] arr, int index, int r, List<Integer> temp) {
+public class findAllCombinations {
 
-    //If we selected r elements -> print
-    if (temp.size() == r) {
-        System.out.println(temp);
-        return;
+    static void findCombinations(int[] arr, int r) {
+        List<Integer> current = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+
+        generate(arr, 0, r, current, result);
+
+        // print result
+        for (List<Integer> comb : result) {
+            System.out.println(comb);
+        }
     }
 
-    //If we reached end -> stop
-    if(index == arr.length) return;
+    private static void generate(int[] arr, int ind, int r,
+                                 List<Integer> current, List<List<Integer>> result) {
 
-    //Take the element
-    temp.add(arr[index]);
-    generate(arr, index+1, r, temp);
+        // if we formed a combination of size r → save it
+        if (current.size() == r) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
 
-    //Remove it(backtrack)
-    temp.remove(temp.size() - 1);
+        // loop through all possible remaining choices
+        for (int i = ind; i < arr.length; i++) {
 
-    //Dont take the element
-    generate(arr,index+1,r,temp);
-}
-public static void main(String[] args) {
-    int[] arr = {1,2,3};
-    printCombinations(arr,2);
-}
+            current.add(arr[i]);           // choose element
+            generate(arr, i + 1, r, current, result); // go deeper
+            current.remove(current.size()-1); // backtrack
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        findCombinations(arr, 2);
+    }
 }
