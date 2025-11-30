932a99433046f4fff652783e5ee99979d26d2987
krutikapatil19 <krutikapatil.1911@gmail.com>
2025-11-30 13:18:11 +0530

    Find all Combinations of size r from an array .

--- Arrays/findAllCombinations.java
+++ Arrays/findAllCombinations.java
@@ -1,27 +1,30 @@
 import java.util.*;
 
 public class findAllCombinations {
-    public static List<List<Integer>> subsets(int[] nums) {
+    public static List<List<Integer>> subsets(int[] nums,int r) {
         List<List<Integer>> result = new ArrayList<>();//create a list called result -> to store subsets.
-        generate(0,nums, new ArrayList<>(), result);//start recursion(we call the helper function to start building subsets )
+        generate(0,nums,r, new ArrayList<>(), result);//start recursion(we call the helper function to start building subsets )
         return result;
     }
-    private static void generate(int index, int[] nums, List<Integer> current, List<List<Integer>>result) {
-
+    private static void generate(int index, int[] nums,int r, List<Integer> current, List<List<Integer>>result) {
+        if (current.size() == r){
         //Save the current subset
         result.add(new ArrayList<>(current));//this ensures we get all subsets
+        return;
+        }
 
         //Try picking every possible next element to add to the subset
         for(int i = index; i<nums.length; i++) {//for ex: if index = 0 -> then try 1, then 2 //If index = 1-> then try 2 only //If index = 2->loop ends
             current.add(nums[i]);//pick the element
-            generate(i+1, nums, current, result);//go deeper
+            generate(i+1, nums, r,current, result);//go deeper
             current.remove(current.size() - 1);//undo pick(backtrack)
         }
     }
     public static void main(String[] args){
         int[] nums = {1,2,3};
+        int r = 2;
 
-        List<List<Integer>> ans = subsets(nums);
+        List<List<Integer>> ans = subsets(nums,r);
 
         //Print the subsets
         for (List<Integer> subset : ans) {
