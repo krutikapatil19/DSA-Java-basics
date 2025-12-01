8c53c450ce0e98d371909463a5e5bb4e53819f63
krutikapatil19 <krutikapatil.1911@gmail.com>
2025-12-01 18:39:10 +0530

    Find all Combinations of size r from an array .

--- Arrays/findAllCombinations.java
+++ Arrays/findAllCombinations.java
@@ -2,21 +2,20 @@ import java.util.*;
 
 public class findAllCombinations {
     public static List<List<Integer>> combinations(int[] nums,int r) {
-        List<List<Integer>> result = new ArrayList<>();//create a list called result -> to store subsets.
+        List<List<Integer>> result = new ArrayList<>();//stores all combinations of size r.
         generate(0,nums,r, new ArrayList<>(), result);//start recursion(we call the helper function to start building subsets )
         return result;
     }
-    private static void generate(int[] nums,int r,int index, List<Integer> current, List<List<Integer>>result) {
+    private static void generate(int index, int[] nums,int r, List<Integer> current, List<List<Integer>>result) {
         if (current.size() == r){
         //Save the current subset
-        result.add(new ArrayList<>(current));//this ensures we get all subsets
-        return;
+        result.add(new ArrayList<>(current));//save this combination of size r
         }
 
         //Try picking every possible next element to add to the subset
         for(int i = index; i<nums.length; i++) {//for ex: if index = 0 -> then try 1, then 2 //If index = 1-> then try 2 only //If index = 2->loop ends
             current.add(nums[i]);//pick the element
-            generate(nums, r,i+1,current, result);//go deeper
+            generate(0, nums, r, new ArrayList<>(), result);//go deeper
             current.remove(current.size() - 1);//undo pick(backtrack)
         }
     }
