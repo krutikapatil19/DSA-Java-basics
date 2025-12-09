681fedbc3013e7fec239603f808bd9f97fb9a65d
krutikapatil19 <krutikapatil.1911@gmail.com>
2025-12-09 22:14:37 +0530

    Valid Palindrome check.

---
+++ Strings/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+
+    public static boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+
+            // skip non-alphanumeric from left
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            // skip non-alphanumeric from right
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            // compare ignoring case
+            if (Character.toLowerCase(s.charAt(left)) 
+                    != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String test1 = "A man, a plan, a canal: Panama";
+        String test2 = "race a car";
+        String test3 = "0P";
+
+        System.out.println(test1 + " → " + isPalindrome(test1));
+        System.out.println(test2 + " → " + isPalindrome(test2));
+        System.out.println(test3 + " → " + isPalindrome(test3));
+    }
+}
