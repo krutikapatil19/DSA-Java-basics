e9a6210be5b71ab323f99e5608fe8268562797c3
krutikapatil19 <krutikapatil.1911@gmail.com>
2025-12-15 22:18:49 +0530

    Group Anagrams.

---
+++ Strings/groupAnagrams.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class groupAnagrams {
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);              // sort characters
+            String key = new String(chars);  // sorted string as key
+
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+
+        List<List<String>> result = groupAnagrams(strs);
+
+        System.out.println(result);
+    }
+}
