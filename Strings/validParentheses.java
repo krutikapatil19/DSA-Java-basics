face1963d9fc1a3d8bfcde2cd34eb96d35b763ea
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-01-01 18:51:49 +0530

    Valid Parenthesis Check.

---
+++ Strings/validParentheses.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+public class validParentheses{
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()) {
+
+            //If opening , push to stack
+            if (c == '(' || c =='{' || c =='[') {
+                stack.push(c);
+            }else {
+                //If closing but no opening to match 
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+
+                //Check matching pairs
+                if(c ==')' && top !='(') return false;
+                if(c =='{' && top != '}') return false;
+                if(c =='[' && top != ']') return false;
+            }
+        }
+
+        //True only if no unclosed brackets left
+        return stack.isEmpty();
+    }
+    public static void main(String[] args){
+        String s = "[({()})]";
+        System.out.println(isValid(s));
+    }
+}
