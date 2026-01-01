81f1eb692d8d5a76424194e74a05bc71eb16a56f
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-01-01 21:36:51 +0530

    Longest common prefix String among Array of strings.

--- Strings/LongestCommonPrefix.java
+++ Strings/LongestCommonPrefix.java
@@ -1,8 +1,8 @@
 //Find the Longest Common Prefix among a group of strings
 public class LongestCommonPrefix {
     public static String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0) return "";                                            //if array is empty , return empty prefix
-        String prefix = strs[0];                                                    //starts with the first word as the prefix
+        if (strs == null || strs.length == 0) return "";                            //if array is empty , return empty prefix
+        String prefix = strs[0];                                                    //take the first string as the prefix or base
 
         for(int i = 1; i< strs.length; i++) {                                       //keep reducing prefix until it matches the start of current word
             while(!strs[i].startsWith(prefix)) {
