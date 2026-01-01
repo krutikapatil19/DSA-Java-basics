b9eec1f8280789093a1f372b2be62449569aae62
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-01-01 19:05:11 +0530

    Remove Consecutive Duplicates from the String.

---
+++ Strings/RemoveConsecutiveDuplicates.java
@@ -0,0 +1,21 @@
+public class RemoveConsecutiveDuplicates {
+    public static String removeConsecutiveChars(String s) {
+
+        //StringBuilder for efficient string building
+        StringBuilder ans = new StringBuilder();
+
+        for(int i = 0; i< s.length(); i++) {
+
+            //add first character OR
+            //add only if current char is different from last added character
+            if(i == 0 || s.charAt(i) != s.charAt(i-1)) {
+                ans.append(s.charAt(i));
+            }
+        }
+        return ans.toString();
+    }
+    public static void main(String[] args){
+        String s = "hashhtagg";
+        System.out.println(removeConsecutiveChars(s));
+    }
+}
