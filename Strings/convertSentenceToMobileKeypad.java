fa0c68f8489585d0837813ee2cb12d7f02ced509
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-01-02 19:14:39 +0530

    Convert a Sentence into its equivalent Mobile Keypad sequence.

--- Strings/convertSentenceToMobileKeypad.java
+++ Strings/convertSentenceToMobileKeypad.java
@@ -13,17 +13,17 @@ public class convertSentenceToMobileKeypad {
             "9","99","999","9999"   //W,X,Y,Z
         };
 
-        StringBuilder output = new StringBuilder();
+        StringBuilder output = new StringBuilder(); //Stringbuilder is fast and efficient in linear time.(As String is immutable in Java, so repeated concatenation is inefficient)
 
         for(char ch : input.toCharArray()) {
             if (ch == ' ') {
                 output.append("0"); //space becomes 0
             } else {
                 int index = ch - 'A';   //find position
-                output.append(map[index]);
+                output.append(map[index]);//Use index to fetch keypad number from map , and append it to the output.
             }
         }
-        return output.toString();
+        return output.toString();   //Convert StringBuilder to String 
     }
     public static void main(String[] args) {
         String input = "HELLO WORLD";
