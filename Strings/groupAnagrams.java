8903646b738c97bf5ce1fa6cd8ebb5890c041b0a
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-01-03 22:10:14 +0530

    Group Anagrams .

--- Strings/groupAnagrams.java
+++ Strings/groupAnagrams.java
@@ -2,18 +2,18 @@ import java.util.*;
 
 public class groupAnagrams {
 
-    public static List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> map = new HashMap<>();
+    public static List<List<String>> groupAnagrams(String[] strs) {         //This is a method that takes an array of strings as input and returns a list of lists.(Each inner list will contain words that are anagrams of each other).
+        Map<String, List<String>> map = new HashMap<>();                    //Here HashMap is created . Key will be a string, and value will be a List<String> that stores all words that match that sorted form.
 
         for (String s : strs) {
-            char[] chars = s.toCharArray();
-            Arrays.sort(chars);              // sort characters
-            String key = new String(chars);  // sorted string as key
+            char[] chars = s.toCharArray();                                 //This converts the current word into a character array.
+            Arrays.sort(chars);                                             // sort characters in array 
+            String key = new String(chars);                                 // sorted string as key
 
-            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);        //It means if the map does not already contain this key, create a new empty list for it. Then add the og word 's' to the list for that key. (So all words with the same sorted key are grouped together).
         }
 
-        return new ArrayList<>(map.values());
+        return new ArrayList<>(map.values());                               //map.values() -> gives all the grouped lists stored in the map. We convert it into an ArrayList and return it.
     }
 
     public static void main(String[] args) {
