65dc1a6b55ed4b94b8ec3db4a05c74c95aef84af
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-01-03 18:57:04 +0530

    Longest Repeating Character Replacement

--- Strings/LongestRepeatingCharacterReplacement.java
+++ Strings/LongestRepeatingCharacterReplacement.java
@@ -1,18 +1,18 @@
 public class LongestRepeatingCharacterReplacement {
     public static int characterReplacement(String s, int k) {
-        int[] freq = new int[26];
+        int[] freq = new int[26];   //frequency of each character from A-Z
         int start = 0;
-        int maxFreq = 0;
+        int maxFreq = 0;            //max frequency in current window 
         int maxLen = 0;
 
-        for(int end = 0;end<s.length(); end++) {
-            char c = s.charAt(end);
-            freq[c-'A']++;
+        for(int end = 0;end<s.length(); end++) {            //expands the window one character at a time 
+            char c = s.charAt(end);                         //Take the current character at position end 
+            freq[c-'A']++;                                  //Increase count of that character in the window 
 
             //update max frequency
-            maxFreq = Math.max(maxFreq, freq[c-'A']);
+            maxFreq = Math.max(maxFreq, freq[c-'A']);       //Update the most frequent character count 
 
-            //if replacement needed>k, shrink window
+            //if replacement needed > k, shrink window
             if ((end-start+1) - maxFreq > k) {
                 freq[s.charAt(start) - 'A']--;
                 start++;
