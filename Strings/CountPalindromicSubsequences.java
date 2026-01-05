f4790b6a057feed6ed7c094f007cd51e7f1fcce4
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-01-05 22:04:29 +0530

    Count Palindromic Subsequences.

---
+++ Strings/CountPalindromicSubsequences.java
@@ -0,0 +1,32 @@
+public class CountPalindromicSubsequences {
+
+    static long countPS(String s) {
+        int n = s.length();
+        long[][] dp = new long[n][n];
+
+        // single characters are palindromes
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+
+        // consider substrings of length 2 to n
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = 1 + dp[i + 1][j] + dp[i][j - 1];
+                } else {
+                    dp[i][j] = dp[i + 1][j] + dp[i][j - 1] - dp[i + 1][j - 1];
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+
+    public static void main(String[] args) {
+        String s = "aaa";
+        System.out.println(countPS(s)); 
+    }
+}
