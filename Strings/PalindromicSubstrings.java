17cee0b26381c6e2c6fa1bd2553ebb344b14d047
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-01-05 19:11:13 +0530

    Count Palindromic Substrings in a String s.

--- Strings/PalindromicSubstrings.java
+++ Strings/PalindromicSubstrings.java
@@ -4,19 +4,19 @@ public class PalindromicSubstrings {
         int count = 0;
 
         for(int i = 0;i<n;i++){
-            //odd-length
+            //odd-length palindromes
             count+= expand(s, i, i);
 
-            //even-length
+            //even-length palindromes
             count += expand(s,i,i+1);
         }
         return count;
     }
-    private static int expand(String s, int left, int right) {
+    private static int expand(String s, int left, int right) {      //expand() func - moves left and right outwards , Each time the chars match , it counts one-palindrome.
         int count = 0;
 
         while(left >= 0 && right <s.length() && s.charAt(left) == s.charAt(right)) {
-            count++;
+            count++;    //found a palindrome 
             left--;
             right++;
         }
@@ -28,3 +28,9 @@ public class PalindromicSubstrings {
         System.out.println(countSubstrings(s));
     }
 }
+
+/* For each index, expand around it(odd and even) and count palindromes.
+Complexities 
+Time: O(N^2)
+Space: O(1)
+*/
