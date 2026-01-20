296992ce950856663f66d3be9b5cce5e052851bb
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-01-20 22:28:07 +0530

    Leetcode 2149 : Rearrange Array Elements by Sign.

---
+++ Arrays/RearrangeElementsBySign.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+public class RearrangeElementsBySign {
+    public static int[] rearrangeElements(int[] nums) {
+
+        //Step 1: Create two arrays/lists for positive and negatives
+
+        ArrayList<Integer> positives = new ArrayList<>();
+        ArrayList<Integer> negatives = new ArrayList<>();
+
+        //2. store positives and negatives separatey
+        for(int i = 0;i<nums.length;i++){
+            if (nums[i] >0){
+                positives.add(nums[i]);
+            } else {
+                negatives.add(nums[i]);
+            }
+        }
+        //3. Create answer array of same size
+        int[] ans = new int[nums.length];
+
+        //4.Fill alternatively:
+        int p = 0, n = 0;
+
+        for(int i = 0; i< nums.length; i++) {
+            if( i% 2 == 0) {
+                ans[i] = positives.get(p);
+                p++;
+            }else{
+                ans[i] = negatives.get(n);
+                n++;
+            }
+        }
+        return ans;
+    }
+
+    
+    public static void main(String[] args) {
+        int[] nums = {3,1,-2,-5,2,-4};
+
+        int[] result = rearrangeElements(nums);
+        System.out.println(Arrays.toString(result));
+    }
+}
