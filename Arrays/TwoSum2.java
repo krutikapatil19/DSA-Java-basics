0fda43d3316b7d095813ae7f84b192d7ef8740f6
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-01-22 22:20:58 +0530

    Two SumII Leetcode (Using Two-Pointer Technique).

--- Arrays/TwoSum2.java
+++ Arrays/TwoSum2.java
@@ -1,13 +1,14 @@
 public class TwoSum2 {
     public static int[] twoSum(int[]nums, int target){
 
-        int n = nums.length;
         int i = 0;
         int j = nums.length-1;
+
         while(i<j){
             int sum = nums[i]+nums[j];
+
             if(sum==target){
-                return new int[]{i,j};
+                return new int[]{i+1,j+1};
             }else if (sum> target){
                 j--;
             } else {
