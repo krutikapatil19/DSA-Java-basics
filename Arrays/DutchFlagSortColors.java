ca8f62c08e9429effa02f6b15dcc7d22a3cbeff0
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-01-26 21:09:56 +0530

    Dutch National Flag Problem.

--- Arrays/DutchFlagSortColors.java
+++ Arrays/DutchFlagSortColors.java
@@ -1,28 +1,31 @@
 import java.util.Arrays;
 public class DutchFlagSortColors {
     public static int[] sortColors(int[] nums) {
-        int low = 0,mid = 0, high = nums.length-1;
+        int low = 0,mid = 0, high = nums.length-1; //low -> wherenext 0 should go , mid -> current element we are checking , high -> where next 2 should come
 
-        while(mid <= high) {
-            if(nums[mid] == 0) {
-                //swap with low
+        while(mid <= high) {    //checking until mid crosses high
+            if(nums[mid] == 0) {    //if current is 0 -> send it to left side
+                //swap nums[low] nad nums[mid]
                 int temp = nums[low];
                 nums[low] = nums[mid];
                 nums[mid]= temp;
 
+                //move both pointers forward
                 low++;
                 mid++;
             }
+            //if current is 1 -> it is already in correct middle position
             else if (nums[mid] == 1) {
                 mid++;
             }
+            //if current is 2 -> send it to right side
             else {
-                //swap with high
+                //swap nums[mid] and nums[high]
                 int temp = nums[mid];
                 nums[mid] = nums[high];
                 nums[high] = temp;
 
-                high--;
+                high--; //move high backward ,mid will not move
             }
         }
         return nums;
