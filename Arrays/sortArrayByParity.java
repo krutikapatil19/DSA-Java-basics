6441b8e11dbcd192437ff577acbdd8df1e37a9b0
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-01-27 21:56:10 +0530

    Sort Array by Parity.

---
+++ Arrays/sortArrayByParity.java
@@ -0,0 +1,29 @@
+public class sortArrayByParity {
+    public static int[] sortArrayByParity(int[] nums){
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left<right){
+
+            if(nums[left]%2 ==0){
+            left++;
+            }
+            else if (nums[right] % 2 == 1){
+                right --;
+            }
+            else {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+        return nums;
+    }
+    public static void main(String[] args){
+        int[] nums = {3,8,7,9,4,2};
+        System.out.println(Arrays.toString(sortArrayByParity(nums)));
+    }
+}
