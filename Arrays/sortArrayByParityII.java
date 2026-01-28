f67eb3ca6d45d2bca4c338d4a472736fe5d09a05
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-01-28 21:55:14 +0530

    Sort Array by ParityII.

---
+++ Arrays/sortArrayByParityII.java
@@ -0,0 +1,30 @@
+public class sortArrayByParityII {
+    public static int[] sortByParityII(int[] nums){
+        int even = 0;
+        int odd = 1;
+
+        while(even<nums.length && odd<nums.length) {
+            if(nums[even] % 2 == 0) {
+                even += 2;
+            }
+            else if (nums[odd]%2 == 1){
+                odd += 2;
+            }
+            else {
+                //swap
+                int temp = nums[even];
+                nums[even] = nums[odd];
+                nums[odd] = temp;
+                even += 2;
+                odd += 2;
+            }
+        }
+        return nums;
+    }
+    public static void main(String[] args){
+        int[] nums = {2,4,5,7};
+
+        System.out.println(sortByParityII(nums));
+    }
+    
+}
