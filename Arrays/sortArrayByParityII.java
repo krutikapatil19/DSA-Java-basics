fec9ce662c434bcfde9d0afa1dfd67a6a789331f
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-01-28 21:59:23 +0530

    Sort Array by ParityII.

--- Arrays/sortArrayByParityII.java
+++ Arrays/sortArrayByParityII.java
@@ -1,18 +1,24 @@
 import java.util.Arrays;
 public class sortArrayByParityII {
     public static int[] sortByParityII(int[] nums){
-        int even = 0;
-        int odd = 1;
+        int even = 0;//even points to even index (0, 2, 4, ...)
+        int odd = 1;// odd points to odd index  (1, 3, 5, ...)
+
 
         while(even<nums.length && odd<nums.length) {
+
+         // if even index has even number, then its in correct place
+
             if(nums[even] % 2 == 0) {
-                even += 2;
+                even += 2;//// move to next even index
             }
+            // if odd index has odd number,then its in correct place
             else if (nums[odd]%2 == 1){
                 odd += 2;
             }
             else {
-                //swap
+            // both positions are wrong -> swap them
+
                 int temp = nums[even];
                 nums[even] = nums[odd];
                 nums[odd] = temp;
