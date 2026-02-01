3ab2a3dcdce2fbae7996b8ff5b5459489b9a0e87
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-02-01 12:02:42 +0530

    Squares Of Sorted Array .(Two -pointers pattern).

--- Arrays/SquaresOfSortedArray.java
+++ Arrays/SquaresOfSortedArray.java
@@ -1,21 +1,26 @@
 import java.util.Arrays;
 public class SquaresOfSortedArray {
     public static int[] squaresOfArray(int[] nums){
-        int i = 0;
-        int j = nums.length -1;
-        int[] result = new int[nums.length];
-        int k = result.length -1;
+        int i = 0;  //i starts from beginning 
+        int j = nums.length -1; //j starts from end 
+
+        int[] result = new int[nums.length];    //new array to store the sorted squares
+        int k = result.length -1;               //k fills the array from the end 
         
-        while(i<=j){
+        while(i<=j){                            //loop until both the pointers meet
+
+            //Calculate squares of both ends
             int squareLeft = nums[i] * nums[i];
             int squareRight = nums[j] *nums[j];
+
+            //Compare both the squares , and place the bigger one at the end of result 
             if(squareLeft < squareRight){
-                result[k] = squareRight;
-                j--;
+                result[k] = squareRight;    //put right square       
+                j--;                        //move right pointer              
                 k--;
             } else {
-                result[k] = squareLeft;
-                i++;
+                result[k] = squareLeft;     //if Left square is greater than right square , put it at kth position
+                i++;                        //move left pointer
                 k--;
             }
         }
@@ -29,5 +34,5 @@ public class SquaresOfSortedArray {
 
 //I solved this problem using the two-pointer approach , in which two pointers are used ,i and j , i will start from 0 ,iterate through the loop ,and j will start from last element.
 
-/*Time Complexity : O(n)
-Space Complexity : O(n)*/
+/*Time Complexity : O(n) - one pass 
+Space Complexity : O(n) - result array */
