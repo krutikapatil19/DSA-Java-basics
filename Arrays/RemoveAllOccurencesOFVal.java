bde81e03f2ab37eb88e46a0e687e4f044ec37fa7
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-02-02 10:34:38 +0530

    Remove All Occurences of Given Value.

---
+++ Arrays/RemoveAllOccurencesOFVal.java
@@ -0,0 +1,21 @@
+public class RemoveAllOccurencesOFVal {
+    public static int removeOccurencesOfNumber(int[] nums, int val){
+
+        int i = 0;
+        int j = 0;
+        
+        for( i = 0;i<nums.length; i++){
+            if(nums[i] != val){
+                nums[j] = nums[i];
+                j++;
+            } else {
+            }
+        }
+        return j;
+    }
+    public static void main(String[] args){
+        int[] nums = {0,1,2,2,0,4,2};
+        int val = 4;
+        System.out.println(removeOccurencesOfNumber(nums,val));
+    }
+}
