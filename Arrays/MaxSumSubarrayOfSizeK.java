3a7bdeb834caa33e9480db99ed4bb33e16201bd5
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-02-11 10:06:20 +0530

    Maximum Subarray Sum of Size k.

--- Arrays/MaxSumSubarrayOfSizeK.java
+++ Arrays/MaxSumSubarrayOfSizeK.java
@@ -7,15 +7,18 @@ public class MaxSumSubarrayOfSizeK {
 
         for(int i = 0;i<k;i++){
             currSum += nums[i];
+            maxSum = currSum;
         }
+        
         for(int i = k;i<nums.length;i++){
             currSum = currSum - nums[i-k] + nums[i];
         }
+        Math.max(currSum, maxSum);
         return maxSum;
     }
     public static void main(String[] args){
         int[] nums = {2,9,31,-4,21,7};
         int k = 3;
-        System.out.println(Arrays.toString(maximumSumContageousArray(nums,k)));
+        System.out.println(maximumSumContageousArray(nums,k));
     }
 }
